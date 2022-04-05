41a1b6d49203653a05f42328e19718544f8def8c
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2022-04-06 01:11:42 +0300

    stray убран

--- ConsoleShapes/Program.cs
+++ ConsoleShapes/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleShapes
             testrectangle1();
             testrhombus();
             testcircle2();
-            Console.WriteLine("stray");
+            
             
 
             
